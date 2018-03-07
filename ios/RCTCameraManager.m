7b7d12107eb384c7dcbc4c3371e458560541b8b0
Mike Ringrose <mringrose@emocha.com>
2018-03-06 21:08:00 -0500

    Setting the proper codec.

--- ios/RCTCameraManager.m
+++ ios/RCTCameraManager.m
@@ -437,8 +437,6 @@ RCT_EXPORT_METHOD(hasFlash:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRej
     AVCaptureMovieFileOutput *movieFileOutput = [[AVCaptureMovieFileOutput alloc] init];
     if ([self.session canAddOutput:movieFileOutput])
     {
-      AVCaptureConnection *movieFileOutputConnection = [movieFileOutput connectionWithMediaType:AVMediaTypeVideo];
-      [movieFileOutput setOutputSettings:@{AVVideoCodecKey : AVVideoCodecH264} forConnection:movieFileOutputConnection];
       [self.session addOutput:movieFileOutput];
       self.movieFileOutput = movieFileOutput;
     }
@@ -765,7 +763,9 @@ RCT_EXPORT_METHOD(hasFlash:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRej
   }
 
   dispatch_async(self.sessionQueue, ^{
-    [[self.movieFileOutput connectionWithMediaType:AVMediaTypeVideo] setVideoOrientation:orientation];
+    AVCaptureConnection *movieFileOutputConnection = [self.movieFileOutput connectionWithMediaType:AVMediaTypeVideo];
+    [movieFileOutputConnection setVideoOrientation:orientation];
+    [self.movieFileOutput setOutputSettings:@{AVVideoCodecKey : AVVideoCodecH264} forConnection:movieFileOutputConnection];
 
     //Create temporary URL to record to
     NSString *outputPath = [[NSString alloc] initWithFormat:@"%@%@", NSTemporaryDirectory(), @"output.mov"];
